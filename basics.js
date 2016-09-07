168b9bed34ce522dff0af543b33e489cc9e24837
Alan Kebert <adkebert@yahoo.com>
2016-09-06 20:38:22 -0400

    Explorer mode complete

---
+++ basics.js
@@ -0,0 +1,22 @@
+// 1. Assign the message "Hello, World!" to a variable.
+var message = 'Hello, World!'
+// 2. Assign a different string to a different variable.
+var a_different_variable = 'a different string'
+// 3. Assign a number to a variable.
+var number = 37
+// console.log("Message: " + message + ", Different variable: " + a_different_variable + ", Number: " + number)
+// 4. Use string concatenation to display the number from #3 in a string.
+console.log('The number is: ' + number)
+// 5. Make an array of at least four of your favorite movies or books or bands.
+var movies = ['Deadpool', 'Interstellar', 'Thor, the Dark World', 'Eurotrip']
+// for(index in movies) {
+//   console.log("Movie: " + movies[index] + " ")
+// }
+// 6. Make a object of information about yourself with at least four properties.
+var alanK = {
+  height: '6"',
+  age: 30,
+  dog: 'Finn',
+  cat: 'Corky'
+}
+// console.log(alanK.height + ", " + alanK.age + ", " + alanK.dog + ", " + alanK.cat)
